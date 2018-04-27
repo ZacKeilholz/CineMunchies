e33ca90040e28a4024f976ad66968c9f56f86ad1
Kalsang Bhutia <35473365+hurlyburly@users.noreply.github.com>
2018-04-26 22:46:20 -0500

    adding in jQuery on submit function to call to APIs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,53 @@
-//JS Setup Functions (Firebase, etc) 
+//JS Setup Functions (Firebase, etc)
 //=================================
 
-
-
-
-//Global Vars 
+//Global Vars
 //=================================
 
-
-
-
-
-
-
-//Jquery HTML Targets 
+//Jquery HTML Targets
 //=================================
 
 //var $exampleTarget = $("#example-container");
 
-
-
-
-
 //Misc. Functions
 //=================================
 
-
-
-
 //JQuery Events / Event Listeners
 //=================================
-$(document).ready(function () {
 
+$(document).ready(function() {
+  $("#search-form").on("submit", function(event) {
+    event.preventDefault();
+    var apiKey = "39a2a8a2";
+    var $search = $("#search-input").val();
+    var omdbURL =
+      "https://www.omdbapi.com/?t=" +
+      $search +
+      "&y=&plot=short&apikey=" +
+      apiKey;
+    var edamamURL = "";
+    var queryURL = "";
+    if ($("#customRadioInline1").is(":checked")) {
+      queryURL = omdbURL;
+    } else if ($("#customRadioInline2").is(":checked")) {
+      queryURL = edamamURL;
+    } else{
+       //FORM VALIDATION INFO?  
+    }
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      var data = response;
+      var newMovie = $("<tr>");
 
+      newMovie
+        .append(`<td scope="row">${data.Title}</td>`)
+        .append(`<td scope="row">${data.Plot}</td>`)
+        .append(`<td scope="row"><img src=${data.Poster}></td>`);
 
-
-    
+      $("tbody").prepend(newMovie);
+      console.log(data.Title, data.Plot);
+    });
+  });
 });
